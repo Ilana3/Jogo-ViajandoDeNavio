b5e285fbd8241df37b2de249f20427ec79c1244b
Ilana3 <98059446+Ilana3@users.noreply.github.com>
2022-02-09 19:08:42 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,20 +1,25 @@
 var ship, ship_animation
+var fundo, fundoImg
 
 function preload(){
-ship_animation = loadAnimation ("ship-1.png", "ship-2.png", "ship-3.png", "ship-4.png");
+  ship_animation = loadAnimation ("ship-1.png", "ship-2.png", "ship-3.png", "ship-4.png");
+  fundoImg = loadImage ("sea.png");
 }
 
 function setup(){
   createCanvas(400,400);
 
-//criando o navio e sua animação
-ship = createSprites(200,200,200,200);
-ship.addAnimation("flutuando", ship_animation);
-ship.scale = 50/100
+  fundo = createSprite(200,200,400,400);
+  fundo.addImage("image de fundo", fundoImg)
+  fundo.scale = 0.3;
+  //criando o navio e sua animação
+  ship = createSprite(150,205,100,100);
+  ship.addAnimation("flutuando", ship_animation);
+  ship.scale = 0.3;
 }
 
 function draw() {
   background("blue");
 
-    drawSprites();
+  drawSprites();
 }
